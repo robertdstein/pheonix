a8ec8acd5b7043a4071e15bf978c13cf97fbc159
Robert Stein <rdstein@caltech.edu>
2023-04-14 18:01:13 -0700

    Fix #1

--- snipergw/__main__.py
+++ snipergw/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+import numpy as np
 
 from snipergw.skymap import Skymap
 from snipergw.plan import run_gwemopt
@@ -23,6 +24,7 @@ parser.add_argument("--exposuretime", default=DEFAULT_EXPOSURE)
 parser.add_argument("--subprogram", default="EMGW")
 parser.add_argument("-c", "--cache", default=False, action="store_true")
 parser.add_argument("-s", "--submit", default=False, action="store_true")
+parser.add_argument("-d", "--delete", default=False, action="store_true")
 args = parser.parse_args()
 
 event = EventConfig(**args.__dict__)
@@ -31,11 +33,13 @@ plan_config = PlanConfig(**args.__dict__)
 skymap = Skymap(event_config=event)
 schedule = run_gwemopt(skymap=skymap, plan_config=plan_config)
 
-if args.submit:
+if np.sum([args.submit is True, args.delete is True]) > 0:
     if args.telescope == "ZTF":
-        submit_too_ztf(schedule, event_config=event, subprogram=args.subprogram)
+        submit_too_ztf(schedule, event_config=event, subprogram=args.subprogram,
+                       submit=args.submit, delete=args.delete)
     else:
         raise NotImplementedError
+
 else:
     logger.info("Rerun with the --submit flag to actually submit a ToO. "
                 "You can additionally use the --cache flag to use the schedule "
--- snipergw/submit/ztf.py
+++ snipergw/submit/ztf.py
@@ -2,14 +2,17 @@ import pandas as pd
 import os
 from planobs.models import TooTarget
 from snipergw.model import EventConfig
-from planobs.api import Queue
+from planobs.api import Queue, APIError
+import time
 
 ZTF_FILTER_MAP = {"g": 1, "r": 2, "i": 3}
 
 def submit_too_ztf(
         schedule: pd.DataFrame,
         event_config: EventConfig,
-        subprogram: str = "EMGW"
+        subprogram: str = "EMGW",
+        submit: bool = False,
+        delete: bool = True
 ):
     """
     Submit a ToO to ZTF
@@ -17,6 +20,8 @@ def submit_too_ztf(
     :param schedule: Schedule dataframe
     :param event_config: Event config
     :param subprogram: Subprogram name e.g EMGW
+    :param submit: Submit the queue
+    :param delete: Delete the queue
     :return: None
     """
     
@@ -38,13 +43,59 @@ def submit_too_ztf(
 
     t_0 = min(schedule["tobs"])
     t_1 = max(schedule["tobs"])
+
+    trigger_name = f"ToO_{subprogram}_{event_config.event}"
     
     q.add_trigger_to_queue(
         targets=targets,
-        trigger_name=f"ToO_{subprogram}_{event_config.name}",
+        trigger_name=trigger_name,
         validity_window_start_mjd=t_0,
         validity_window_end_mjd=t_1,
     )
 
-    print(targets)
+    expected_name = f"{trigger_name}_0"
+
+    def in_queue() -> bool:
+        current_too_queue = q.get_too_queues()
+
+        print(
+            f"Current Kowalski queue has "
+            f"{len(current_too_queue['data'])} entries"
+        )
+
+        kowalski_list = [
+            current_too_queue["data"][i]["queue_name"]
+            for i in range(len(current_too_queue["data"]))
+        ]
+
+        return expected_name in kowalski_list
+
+
+    if submit:
+
+        try:
+            q.delete_queue()
+        except APIError:
+            pass
+    
+        # Now we submit our triggers
+        q.submit_queue()
+    
+        time.sleep(5)
+
+        if not in_queue():
+            raise RuntimeError("Trigger not added to queue")
+
+    if delete:
+        print("deleting queue")
+        if not in_queue():
+            raise RuntimeError("Trigger not in queue")
+        # Now we delete our triggers
+        q.delete_queue()
+    
+        current_too_queue = q.get_too_queues()
+    
+        print(
+            f"Current Kowalski queue has {len(current_too_queue['data'])} entries (after deleting)"
+        )
 
